90f3f8538f03b90bae14fcaea0eeb992a7e7dcb0
kpwahn <kpwahnschaffe@gmail.com>
2015-05-11 09:38:34 -0600

    Changes dir name

---
+++ assignment01/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<h2>Who Is Your Favorite Harry Potter Character?</h2>
+<body>
+  <form name="myForm" method="post" action="survey.php">
+    <input type="radio" name="character" value="0"/> <label>Serverus Snape</label> <br />
+    <input type="radio" name="character" value="1"/> <label>Harry Potter</label> <br />
+    <input type="radio" name="character" value="2"/> <label>Fred Weasley</label> <br />
+    <input type="radio" name="character" value="3"/> <label>Dobby</label> <br />
+    <input type="radio" name="character" value="4"/> <label>Rubeus Hagrid</label> <br />
+    <input type="radio" name="character" value="5"/> <label>Minerva McGonagall</label> <br />
+    <input type="radio" name="character" value="6"/> <label>Albus Percival Wulfric Brian Dumbledore</label> <br />
+    <button type="submit">Submit</button>
+  </form>
+</body>
+</html>
