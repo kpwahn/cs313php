ad1d1b34e7caa389e94344ced08726fb6df5e92d
kpwahn <kpwahnschaffe@gmail.com>
2015-05-18 16:41:37 -0600

    activity 4

--- teamActivity04.php
+++ teamActivity04.php
@@ -1,11 +1,14 @@
 <?php
 
-$server = $OPENSHIFT_scriptures_DB_HOST
-$database = 'scriptures';
-$username = 'adminSPemDYX';
-$password = 'tnefpMRrBe7g';
 
-$sdb = new PDO("mysql: host=$server; dbname=$database", $username, $password);
+define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT')); 
+define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('DB_NAME',getenv('OPENSHIFT_GEAR_NAME'));
+
+$dsn = 'mysql:dbname='.DB_NAME.';host='.DB_HOST.';port='.DB_PORT;
+$sdb = new PDO($dsn, DB_USER, DB_PASS);
 
 echo "<h2> Scripture Resources </h2>";
 
