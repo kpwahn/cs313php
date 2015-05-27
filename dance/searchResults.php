abccef90b15ff3f2da111d57be91b8e71e70069c
kpwahn <kpwahnschaffe@gmail.com>
2015-05-27 15:23:02 -0600

    flskjfsd

--- dance/searchResults.php
+++ dance/searchResults.php
@@ -33,11 +33,12 @@ $database = 'dance';
 $username = 'root';
 $password = '';
 
-$sdb = new PDO("mysql: host=$server; dbname=$database", $username, $password);
+//$sdb = new PDO("mysql:host=" . DB_HOST . ";dbname={$dbname};charset=utf8", DB_USER, DB_PASS, $options); 
+//$sdb = new PDO("mysql: host=$server; dbname=$database", $username, $password);
 
-foreach($sdb->query("select style_id from style_table where style = '$style'") as $row) {
+foreach($db->query("select style_id from style_table where style = '$style'") as $row) {
   $temp = $row["style_id"];
-  foreach($sdb->query("select * from move_table where style = '$temp'") as $row1) {
+  foreach($db->query("select * from move_table where style = '$temp'") as $row1) {
     $temp2 = $row1['url'];
     echo $row1["move_name"] . ": <br /> <iframe width='854' height='510' src=\"$temp2\" frameborder='0' allowfullscreen></iframe>";
   }
